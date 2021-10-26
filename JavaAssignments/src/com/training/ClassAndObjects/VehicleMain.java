0c80a623e216b6659ddf4680d10c1bb3dba79ff1
Sabbujaiswal <92732115+Sabbujaiswal@users.noreply.github.com>
2021-10-26 19:52:15 +0530

    Update VehicleMain.java

--- JavaAssignments/src/com/training/ClassAndObjects/VehicleMain.java
+++ JavaAssignments/src/com/training/ClassAndObjects/VehicleMain.java
@@ -5,17 +5,17 @@ public class VehicleMain {
 	public static void main(String[] args) {
 		
 		Vehicle v = new Vehicle();
-		v.name = "Ford";
-		v.model = "Mustang";
-		v.price = 9000000;
+		v.name = "TATA";
+		v.model = "Swift";
+		v.price = 500000;
 		v.getDetails();
 		
 		System.out.println();
 		
 		Vehicle v1 = new Vehicle();
-		v1.name = "Tata";
-		v1.model = "Altroz";
-		v1.price = 90000;
+		v1.name = "BMW";
+		v1.model = "Nexon";
+		v1.price = 450000;
 		v1.getDetails();
 	}
 
