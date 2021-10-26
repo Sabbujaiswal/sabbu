bae5fe3d96983252f9e2de2a77c218153c5e7077
Sabbujaiswal <92732115+Sabbujaiswal@users.noreply.github.com>
2021-10-26 22:45:28 +0530

    Update Overload_main.java

--- JavaAssignments/src/com/shristi/polymorphism/Overload_main.java
+++ JavaAssignments/src/com/shristi/polymorphism/Overload_main.java
@@ -3,15 +3,17 @@ package com.shristi.polymorphism;
 public class OverloadMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Polymorphism manager = new Polymorphism("sabbu", "Manager");
-		System.out.println(manager.calcBonus(1000, 1000));
 		
-		Polymorphism programmer = new Polymorphism("sacin", "Programmer");
-		System.out.println(programmer.calcBonus(1000));
 		
-		Polymorphism director = new Polymorphism("jaiswal", "Director");
-		System.out.println(director.calcBonus(1000, 1000, 1000));
+		Employee programmer = new Employee("sachin", "Programmer");
+		System.out.println(programmer.calcBonus(5000));
+		
+		Employee manager = new Employee("sabbu", "Manager");
+		System.out.println(manager.calcBonus(5000, 4000));
+		
+		
+		Employee director = new Employee("jaiswal", "Director");
+		System.out.println(director.calcBonus(5000, 4000, 1000));
 		
 		
 		
