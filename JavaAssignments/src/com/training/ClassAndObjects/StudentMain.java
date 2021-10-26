4778fae0d392857855e95483af3e04069e10e061
Sabbujaiswal <92732115+Sabbujaiswal@users.noreply.github.com>
2021-10-26 22:07:40 +0530

    Update StudentMain.java

--- JavaAssignments/src/com/training/ClassAndObjects/StudentMain.java
+++ JavaAssignments/src/com/training/ClassAndObjects/StudentMain.java
@@ -4,13 +4,13 @@ public class StudentMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[] { 10, 20, 30 };
+		int[] arr = new int[] { 20, 40, 60 };
 
-		Student s = new Student("nayeem", "170");
+		Student s = new Student("jaiswl", "160");
 		s.getDetails();
 		s.getGrades(arr);
 		
-		int[] arr1 = new int[] { 15, 25,5 };
+		int[] arr1 = new int[] { 5, 15,25 };
 		
 		System.out.println();
 
